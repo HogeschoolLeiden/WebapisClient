77fc90b1332aea612a510df6a3c586f4e2941eee
Jacqueline <kok.j@hsleiden.nl>
2013-07-29 14:16:40 +0200

    implemented paging

--- src/main/java/nl/hsleiden/webapisclient/Student.java
+++ src/main/java/nl/hsleiden/webapisclient/Student.java
@@ -5,17 +5,22 @@
 package nl.hsleiden.webapisclient;
 
 import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+import java.util.logging.Level;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.core.MultivaluedMap;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
+import org.apache.log4j.Logger;
 
 /**
  *
@@ -27,6 +32,8 @@ public class Student extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
+        org.apache.log4j.Logger logger = org.apache.log4j.Logger.getLogger(InsertImage.class.getName());
+
         request.setCharacterEncoding("UTF-8");
         String achternaam = request.getParameter("achternaam");
         if (achternaam == null || achternaam.trim().isEmpty()) {
@@ -35,28 +42,33 @@ public class Student extends HttpServlet {
 
         Client c = new Client();
         Properties props = new java.util.Properties();
-        
         InputStream in = Student.class.getResourceAsStream("/webapis.properties");
         props.load(in);
-        
-        WebResource  resource = c.resource(props.getProperty("studenturl") + "/" + achternaam);
-        WebResource.Builder builder = resource.header("Range", "Tada!");
-       
-        String result = builder.accept("application/json").get(String.class);
-        
+
+        WebResource resource = c.resource(props.getProperty("studenturl") + "/" + achternaam);
+        MultivaluedMap<String, String> params = new MultivaluedMapImpl();
+        params.add("max", "2");
+        params.add("offset", "0");
+
+        resource.accept("application/json");
+        String result = resource.queryParams(params).get(String.class);
         JSONObject jsonObject = (JSONObject) JSONSerializer.toJSON(result);
+
         //bepaal of er 1 object is gevonden of een array van objecten 
-        if (jsonObject.get("students") instanceof JSONObject) {
+        //Result result = jsonObject.get("result");
+        if (jsonObject.get("students") instanceof JSONArray) {
+            JSONArray arr = (JSONArray) jsonObject.get("students");
+            String next = (String) jsonObject.get("next");
+            String previous = (String) jsonObject.get("previous");
+            request.setAttribute("persons", arr);
+            request.setAttribute("next", next);
+            request.setAttribute("previous", previous);
+        } else if (jsonObject.getJSONObject("students") instanceof JSONObject) {
             JSONObject person = jsonObject.getJSONObject("students");
             request.setAttribute("person", person);
-        } else if (jsonObject.get("students") instanceof JSONArray) {
-            JSONArray arr = JSONArray.fromObject(jsonObject.getJSONArray("students"));
-            request.setAttribute("persons", arr);
         }
-        response.setHeader("Range", "tada!" );
         response.setContentType("text/html;charset=UTF-8");
         request.getRequestDispatcher("toonzoekresultaat.jsp").forward(request, response);
 
     }
-    
 }
