583bb84a1d27d7dd6ea467baddcfb5972b412b3a
Jacqueline <kok.j@hsleiden.nl>
2013-07-29 16:00:12 +0200

    added logstatement

--- src/main/java/nl/hsleiden/webapisclient/Student.java
+++ src/main/java/nl/hsleiden/webapisclient/Student.java
@@ -5,13 +5,11 @@
 package nl.hsleiden.webapisclient;
 
 import com.sun.jersey.api.client.Client;
-import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.core.util.MultivaluedMapImpl;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
-import java.util.logging.Level;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -20,7 +18,6 @@ import javax.ws.rs.core.MultivaluedMap;
 import net.sf.json.JSONArray;
 import net.sf.json.JSONObject;
 import net.sf.json.JSONSerializer;
-import org.apache.log4j.Logger;
 
 /**
  *
@@ -53,7 +50,7 @@ public class Student extends HttpServlet {
         resource.accept("application/json");
         String result = resource.queryParams(params).get(String.class);
         JSONObject jsonObject = (JSONObject) JSONSerializer.toJSON(result);
-
+        logger.debug("Result: " + result);
         //bepaal of er 1 object is gevonden of een array van objecten 
         //Result result = jsonObject.get("result");
         if (jsonObject.get("students") instanceof JSONArray) {
